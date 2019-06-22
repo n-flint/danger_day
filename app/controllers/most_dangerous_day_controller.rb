796114b3cd7f4c2825dec682d42ab94a4996b043
Noah Flint <noah.flint24@gmail.com>
2019-06-22 13:17:26 -0600

    added facade.new to index action

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,4 +1,8 @@
 class MostDangerousDayController < ApplicationController
   def index
+    # require 'pry'; binding.pry
+    render locals: {
+      facade: DangerousDayFacade.new(params['start_date'], params['end_date'])
+    }
   end
 end
